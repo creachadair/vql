ce892849196bc719f4660d8f16a900d99068beb3
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-04-24 23:00:16 -0700

    Make the select example less pathetic.

--- example_test.go
+++ example_test.go
@@ -9,9 +9,9 @@ import (
 
 func ExampleEval() {
 	type Person struct {
-		Name string
-		Exec bool
-		Age  int
+		Name  string
+		Title string
+		Age   int
 	}
 	type Company struct {
 		Name   string
@@ -21,21 +21,22 @@ func ExampleEval() {
 	input := Company{
 		Name: "Stuff, Inc.",
 		People: []*Person{
-			{Name: "Alice", Exec: true, Age: 35},
-			{Name: "Bob", Exec: false, Age: 38},
-			{Name: "Carol", Exec: false, Age: 19},
-			{Name: "Dave", Exec: true, Age: 49},
-			{Name: "Eve", Exec: false, Age: 27},
-			{Name: "Frank", Exec: false, Age: 31},
+			{Name: "Alice", Title: "CEO", Age: 35},
+			{Name: "Bob", Title: "MGR", Age: 38},
+			{Name: "Carol", Title: "MGR", Age: 19},
+			{Name: "Dave", Title: "CFO", Age: 49},
+			{Name: "Eve", Title: "EMP", Age: 27},
+			{Name: "Frank", Title: "EMP", Age: 31},
 		},
 	}
 
-	// A query to select the names of executives, identified by having their
-	// Exec field set true.
+	// A query to select the names of executives, identified by their title
+	// being "CEO" or "CFO".
 	execNames := vql.Seq{
 		vql.Key("People"),
-		vql.Select(vql.Key("Exec"), func(obj interface{}) bool {
-			return obj.(bool)
+		vql.Select(vql.Key("Title"), func(obj interface{}) bool {
+			s := obj.(string)
+			return s == "CEO" || s == "CFO"
 		}),
 		vql.Each(vql.Key("Name")),
 	}
