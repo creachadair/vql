d4acda1714c0c3aa116e0b38ad0337504307c2ea
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-04-24 20:32:59 -0700

    Add initial implementation of vql and unit tests.

---
+++ vql.go
@@ -0,0 +1,175 @@
+// Package vql implements a reflective query interface to traverse Go values.
+// Given a query q and a value v, vql.Eval(q, v) returns the object described
+// by the query starting from v, or an error.
+//
+// TODO: This package needs much more documentation.
+//
+// TODO: Implement additional interesting query operators.
+package vql
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Eval evaluates q starting from v, and returns the object reached.
+func Eval(q Query, v interface{}) (interface{}, error) {
+	result, err := q.eval(newValue(v))
+	if err != nil {
+		return nil, err
+	}
+	return result.val, nil
+}
+
+// A value carries a value through a query, encapsulating the value and the
+// parent value from which it was produced.
+type value struct {
+	val    interface{}
+	parent *value
+}
+
+// newValue constructs a value for obj with no parent.
+func newValue(obj interface{}) *value { return &value{val: obj} }
+
+// pushValue constructs a new value for obj with v as its parent.
+func pushValue(v *value, obj interface{}) *value {
+	return &value{val: obj, parent: v}
+}
+
+// A Query evalutes a query starting at the specified value, returning the
+// resultant value reached by the query.
+type Query interface {
+	eval(*value) (*value, error)
+}
+
+// Self is query whose value is its input.
+var Self selfQuery
+
+type selfQuery struct{}
+
+func (selfQuery) eval(v *value) (*value, error) { return v, nil }
+
+// Const returns a Query whose value is the fixed constant obj.
+func Const(obj interface{}) Query { return constQuery{newValue(obj)} }
+
+type constQuery struct{ *value }
+
+func (c constQuery) eval(v *value) (*value, error) { return c.value, nil }
+
+// Seq is a Query that sequentially composes other Queries.  An empty Seq
+// yields its input unmodified; otherwise the result from the first Query is
+// recursively traversed by those remaining in left to right order.
+type Seq []Query
+
+func (s Seq) eval(v *value) (*value, error) {
+	for _, elt := range s {
+		next, err := elt.eval(v)
+		if err != nil {
+			return v, err
+		}
+		v = next
+	}
+	return v, nil
+}
+
+// Key returns a Query that returns the value of the specified field on a
+// struct, or entry in a map with string keys, or nil if no such field or key
+// exists. It is an error if the value type is not a struct or string-key map.
+func Key(s string) Query { return keyQuery(s) }
+
+type keyQuery string
+
+var stringType = reflect.TypeOf("string")
+
+func (k keyQuery) eval(v *value) (*value, error) {
+	rv := reflect.Indirect(reflect.ValueOf(v.val))
+	var f reflect.Value
+	if rv.Kind() == reflect.Struct {
+		f = rv.FieldByName(string(k))
+	} else if rv.Kind() == reflect.Map && rv.Type().Key() == stringType {
+		f = rv.MapIndex(reflect.ValueOf(string(k)))
+	} else {
+		return nil, fmt.Errorf("value of type %T is not a struct or string map", v.val)
+	}
+	if !f.IsValid() {
+		return pushValue(v, nil), nil
+	}
+	return pushValue(v, f.Interface()), nil
+}
+
+// Each returns a Query that applies v to each element of an array or slice,
+// and yields a slice (of type []interface{}) containing the resulting values.
+func Each(v Query) Query { return mapQuery{v} }
+
+type mapQuery struct{ Query }
+
+func (m mapQuery) eval(v *value) (*value, error) {
+	var vs []interface{}
+	err := forEach(v.val, func(obj interface{}) error {
+		next, err := m.Query.eval(pushValue(v, obj))
+		if err == nil {
+			vs = append(vs, next.val)
+		}
+		return err
+	})
+	return pushValue(v, vs), err
+}
+
+// Select returns a Query that applies f to result of evaluating q for each
+// entry in an array or slice, and yields a slice of concrete type
+// []interface{} containing the values for which f reports true.
+func Select(q Query, f func(interface{}) bool) Query { return selectQuery{q, f} }
+
+type selectQuery struct {
+	Query
+	keep func(interface{}) bool
+}
+
+func (s selectQuery) eval(v *value) (*value, error) {
+	var vs []interface{}
+	err := forEach(v.val, func(obj interface{}) error {
+		v, err := s.Query.eval(pushValue(v, obj))
+		if err != nil {
+			return err
+		} else if s.keep(v.val) {
+			vs = append(vs, obj)
+		}
+		return nil
+	})
+	return pushValue(v, vs), err
+}
+
+func forEach(v interface{}, f func(interface{}) error) error {
+	rv := reflect.ValueOf(v)
+	if k := rv.Kind(); k != reflect.Array && k != reflect.Slice {
+		return fmt.Errorf("value of type %T is not an array or slice", v)
+	}
+	for i := 0; i < rv.Len(); i++ {
+		if err := f(rv.Index(i).Interface()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Bind returns a Query that binds the values from the specified subqueries to
+// the corresponding keys in a string-to-value map.  The concrete type of the
+// result is map[string]interface{}. It is not an error for requested values to
+// be missing; their corresponding values will be nil.
+func Bind(m map[string]Query) Query { return bindQuery(m) }
+
+type bindQuery map[string]Query
+
+func (b bindQuery) eval(v *value) (*value, error) {
+	result := make(map[string]interface{})
+	for key, q := range b {
+		val, err := q.eval(v)
+		if err != nil {
+			return nil, fmt.Errorf("evaluating subquery %q: %v", key, err)
+		}
+		result[key] = val.val
+	}
+	return pushValue(v, result), nil
+}
+
+// TODO: Nicer error messages.
---
+++ vql_test.go
@@ -0,0 +1,84 @@
+package vql_test
+
+import (
+	"strings"
+	"testing"
+
+	"bitbucket.org/creachadair/vql"
+	"github.com/kylelemons/godebug/pretty"
+)
+
+func TestQueries(t *testing.T) {
+	type thingy struct {
+		A string
+		B int
+		S []string
+		T *thingy
+	}
+	t2 := &thingy{ // N.B. pointer
+		A: "bar",
+		B: 25,
+		S: []string{"apple", "pie"},
+		T: nil,
+	}
+	t1 := thingy{ // N.B. non-pointer
+		A: "foo",
+		B: 17,
+		S: []string{"pear", "plum", "cherry"},
+		T: t2,
+	}
+	m := map[string]string{
+		"oh":   "bother",
+		"said": "pooh",
+	}
+
+	tests := []struct {
+		query       vql.Query
+		input, want interface{}
+	}{
+		{vql.Self, "whatever", "whatever"},
+		{vql.Self, nil, nil},
+
+		{vql.Const(true), nil, true},
+		{vql.Const(true), "whatever", true},
+		{vql.Const(125), []string{"a", "b", "c"}, 125},
+
+		{vql.Key("A"), t1, "foo"},
+		{vql.Key("B"), t1, 17},
+		{vql.Key("S"), t1, []string{"pear", "plum", "cherry"}},
+		{vql.Key("C"), t1, nil},
+		{vql.Key("oh"), m, "bother"},
+		{vql.Key("piglet"), m, nil},
+
+		{vql.Seq(nil), "whatever", "whatever"},
+		{vql.Seq{}, "whatever", "whatever"},
+		{vql.Seq{vql.Const(1)}, "whatever", 1},
+		{vql.Seq{vql.Key("T"), vql.Key("A")}, t1, "bar"},
+		{vql.Seq{vql.Key("T"), vql.Key("B")}, t1, 25},
+		{vql.Seq{vql.Key("T"), vql.Key("C")}, t1, nil},
+		{vql.Seq{vql.Key("T"), vql.Key("T")}, t1, (*thingy)(nil)},
+
+		{vql.Each(vql.Key("A")), []*thingy{&t1, t2}, []interface{}{"foo", "bar"}},
+
+		{vql.Seq{
+			vql.Key("S"),
+			vql.Select(vql.Self, func(obj interface{}) bool {
+				s, ok := obj.(string)
+				return ok && strings.HasPrefix(s, "p")
+			}),
+		}, t1, []interface{}{"pear", "plum"}},
+
+		{vql.Bind(map[string]vql.Query{
+			"first":  vql.Key("B"),
+			"second": vql.Seq{vql.Key("T"), vql.Key("B")},
+		}), t1, map[string]interface{}{"first": 17, "second": 25}},
+	}
+	for _, test := range tests {
+		got, err := vql.Eval(test.query, test.input)
+		if err != nil {
+			t.Errorf("Eval(%v): unexpected error: %v", test.query, err)
+		} else if diff := pretty.Compare(got, test.want); diff != "" {
+			t.Errorf("Eval(%v): (-got, +want)\n%s", test.query, diff)
+		}
+	}
+}
