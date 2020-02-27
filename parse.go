849c8bb3523c7896c4e5bf682f1486587d58d542
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-02-27 08:34:33 -0800

    WIP: Parser

---
+++ parse.go
@@ -0,0 +1,49 @@
+package vql
+
+/*
+Query grammar:
+
+query  = alt
+alt    = seq | seq "//" alt
+alts   = alt | alt "," alts   -- 1 or more
+seq    = term | term "." seq
+term   = atom | list | cat | map | each | select | index
+list   = "[" alts? "]"
+cat    = "#[" alts? "]"
+map    = "{" kvals "}"
+kvals  = kval | kval "," kvals
+kval   = key ":" alt
+each   = "each" alt
+select = "select" alt
+index  = alt "[" ikey "]"
+atom   = const | name | "(" alt ")"
+const  = string | int | float | bool
+key    =
+ikey   =
+
+
+
+seq = 
+cat = "#[" seq "]"
+list = "[" seq "]"
+
+type selfQuery struct{}
+type Seq []Query
+type Map map[string]Query
+type Or []Query
+type List []Query
+type Cat []Query
+func Const(obj interface{}) Query { return constQuery{newValue(obj)} }
+func Key(keys ...interface{}) Query {
+func Each(q Query) Query { return mapQuery{q} }
+func Select(q ...Query) Query { return selectQuery{Seq(q)} }
+func Func(v interface{}) Query {
+func Index(i int) Query { return indexQuery(i) }
+func Eq(needle interface{}) Query {
+func Lt(needle interface{}) Query {
+func Le(needle interface{}) Query {
+func Gt(needle interface{}) Query {
+func Ge(needle interface{}) Query {
+func IsNil(obj interface{}) bool { return obj == nil }
+func NotNil(obj interface{}) bool { return obj != nil }
+*/
